fc496ab62b020f45916b8e98b2023925d21597d9
Andressa Silva <andressaszx@gmail.com>
2022-08-30 21:23:47 -0300

    Project created

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Busca de Ações'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
